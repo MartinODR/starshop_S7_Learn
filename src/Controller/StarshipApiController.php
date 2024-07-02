b59448a823c1ff1890ec3017cfa80cf6e23ce639
MartinODR <martinsteinturm@gmail.com>
2024-07-03 00:34:39 +0200

    Implementacion JSON

---
+++ src/Controller/StarshipApiController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Modell\Starship;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class StarshipApiController extends AbstractController
+{
+    #[Route('/api/starships')]
+    public function getCollection(): Response
+    {
+        $starships = [
+            new Starship(
+                1,
+                'USS LeafyCruiser (NCC-0001)',
+                'Garden',
+                'Jean-Luc Pickles',
+                'taken over by Q',
+            ),
+            new Starship(
+                2,
+                'USS Espresso (NCC-0001)',
+                'Latte',
+                'James T. Quick!',
+                'Repaired',
+            ),
+            new Starship(
+                3,
+                'USS Wanderlust  (NCC-2024-W)',
+                'Delta Tourist',
+                'Kathryn Journeyway',
+                'Under Construction',
+            ),        
+        ];
+
+        return $this->json($starships);
+
+    }
+}
