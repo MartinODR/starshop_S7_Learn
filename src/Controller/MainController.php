47aa50d7adba907d8a568ba377910a5bf631b583
MartinODR <martinsteinturm@gmail.com>
2024-07-02 21:27:26 +0200

    Adding TWIG

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,18 +2,28 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
-
     #[Route('/')]
     public function homepage(): Response
     {
+        $starshipCount = 457;
 
-        return new Response('<strong>Starshop</strong>: your monopoly-busting option for Starship parts!');    //[Symfony\Component\HttpFoundation]
+        $myShip = [
+            'name' => 'USS LeafyCruiser (NCC-0001)',
+            'class' => 'Garden',
+            'captain' => 'Jean-Luc Pickles',
+            'status' => 'under construction',
+        ];
 
+        return $this->render('main/homepage.html.twig', [
+            'numberOfStarships' => $starshipCount,
+            'myShip' => $myShip,
+        ]);
+        
     }
-
 }
